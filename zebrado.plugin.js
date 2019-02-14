491caf0c174620e24e412c2bd582d29c9deb7f98
Carol <anacssnts@gmail.com>
2019-02-14 19:16:56 -0200

    inserir a cor que eu quero no zebrado no html e não no plugin

--- zebrado.plugin.js
+++ zebrado.plugin.js
@@ -1,15 +1,14 @@
 $(function(){
 
-    var colorA = "cyan";
-    var colorB = "silver";
+    
 
-    $.fn.zebrado = function(){
+    $.fn.zebrado = function(colorA,colorB){
         // console.log(this);
         $(this)
         .find("tr:even td") 
-        .css("background-color", colorA)
-        .find("tr:odd td")
-        .css("background", colorB);
+        .css("background-color", colorA).end()
+        .find("tr:odd td") 
+        .css("background-color", colorB)
         return this;
     }
 });
